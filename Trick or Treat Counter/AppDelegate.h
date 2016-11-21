e0794c8b7ebf39cff564bce442b9011b1e9f6ed5
Christopher Weaver <crweaver225@yahoo.com>
2016-11-21 12:00:26 -0600

    Initial Commit

---
+++ Trick or Treat Counter/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Trick or Treat Counter
+//
+//  Created by Christopher Weaver on 11/21/16.
+//  Copyright © 2016 Christopher Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
